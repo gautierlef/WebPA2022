3fcde8b4cccfc8f1ca1829f22810b5cb33f395aa
gautierlef <gautier.leffebvre@gmail.com>
2022-05-30 01:02:16 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request, url_for, redirect, current_app, send_from_directory, Response
+import mysql.connector
+import boto3
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('viewHome.html')
+
+
+@app.route('/viewTwitts', methods=['GET'])
+def viewTwitts():
+    twitts = []
+    storage = Storage()
+    data = storage.loadAll()
+    for row in data:
+        twitts.append({'id': str(row[0]), 'nom': row[1], 'description': row[2], 'heures': str(row[3])})
+    return render_template('viewTwitts.html', twitts=twitts)
+
+
+@app.route('/viewArticles', methods=['GET'])
+def viewArticles():
+    articles = []
+    storage = Storage()
+    data = storage.loadAll()
+    for row in data:
+        articles.append({'id': str(row[0]), 'nom': row[1], 'description': row[2], 'heures': str(row[3])})
+    return render_template('viewArticles.html', articles=articles)
+
+
+@app.route('/inputComparison', methods=['GET'])
+def inputComparison():
+    return render_template('viewInputComparison.html')
+
+
+@app.route('/comparison', methods=['POST'])
+def comparison():
+    keyWord = request.form['keyWord']
+    return render_template('viewComparison.html', pseudo=pseudo, recommendedGames=recommendedGames, recommendedPlayers=recommendedPlayers)
+
+
+@app.route("/downloadCSV")
+def downloadCSV():
+    s3client = boto3.client("s3")
+    s3 = boto3.resource(
+        service_name="s3",
+        region_name="us-east-1",
+        aws_access_key_id="AKIAZMSAVZUGSJ7DUL5J",
+        aws_secret_access_key="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"
+    )
+    parielaws1 = s3.Bucket('parielaws1')
+    for obj in parielaws1.objects.all():
+        path, filename = os.path.split(obj.key)
+        parielaws1.download_file(obj.key, filename)
+    with open("matieres.csv") as f:
+        csv = f.read()
+    f.close()
+    os.remove("matieres.csv")
+    return Response(csv, mimetype="text/csv", headers={"Content-disposition": "attachment; filename=matieres.csv"})
+
+
+class Storage:
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            user='AdminPartiel',
+            passwd='PARTIELaws1#',
+            db='Partieldatabase',
+            host='partielrds.c46hgutu5jsv.us-east-1.rds.amazonaws.com',
+            port=3306
+        )
+
+    def loadAll(self):
+        cur = self.db.cursor()
+        cur.execute(''' SELECT id, nom, description, heures FROM Matieres ''')
+        data = cur.fetchall()
+        return data
+
+    def loadMatiere(self, idMatiere):
+        cur = self.db.cursor()
+        cur.execute(''' SELECT id, nom, description, heures FROM Matieres WHERE id = %s ''', (idMatiere, ))
+        matiere = cur.fetchall()
+        return matiere
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
