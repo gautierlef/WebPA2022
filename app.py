e7ad6176ee3db4a88cd45a8fb851ec4f4010edcd
gautierlef <gautier.leffebvre@gmail.com>
2022-06-01 00:21:41 +0200

    Bug fix

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def scrapTweets(word):
     df1 = pd.DataFrame.from_dict(results1["data"])
     df1["link"] = link
     df1["keyword"] = word
-    df = df.concat(df1)
+    df = df.append(df1)
     df = df.reset_index(drop=True)
     df.to_excel("tweetbase.xlsx", index=False)
     return redirect('/')
