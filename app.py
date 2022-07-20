a5ebc2d42e9f08f5c93de213abca6c9f321c9606
gautierlef <gautier.leffebvre@gmail.com>
2022-07-20 03:00:42 +0200

    Bug fixes

--- app.py
+++ app.py
@@ -62,10 +62,8 @@ def inputComparisonArticles(idArticle):
 @app.route('/comparisonById/<idTweet>/<idArticle>', methods=['POST'])
 def comparisonById(idTweet, idArticle):
     storage = Storage()
-    tweetData = storage.loadTweet(idTweet)
-    articleData = storage.loadArticle(idArticle)
-    print(articleData)
-    print(tweetData)
+    tweetData = storage.loadTweet(idTweet)[0]
+    articleData = storage.loadArticle(idArticle)[0]
     tweet = {'id': str(tweetData[0]), 'authorId': str(tweetData[1]), 'date': str(tweetData[2]), 'lang': tweetData[3], 'link': tweetData[3], 'text': tweetData[4]}
     article = {'id': str(articleData[0]), 'title': articleData[1], 'link': str(articleData[2]), 'lang': articleData[3], 'link': articleData[3], 'text': articleData[4], 'tag': articleData[5]}
     print(tweet)
