de38f9828e3e79b3d4986f163782c3fbafd197f0
gautierlef <gautier.leffebvre@gmail.com>
2022-07-21 03:03:35 +0200

    Remove duplicated tweet during addTweets

--- app.py
+++ app.py
@@ -282,10 +282,12 @@ class Storage:
     def addTwittsFromDf(self, df):
         df = df.reset_index()
         cur = self.db.cursor()
+
         cur.execute('''DELETE FROM Tweet WHERE date LIKE %s''', ("\'" + date.today().strftime("%Y-%m-%d") + "%", ))
         for index, row in df.iterrows():
             cur.execute(''' INSERT INTO Tweet(authorId, date, lang, link, text) VALUES (%s, %s, %s, %s, %s) '''
                         , (row['author_id'], row['created_at'], row['lang'], row['link'], row['text']))
+        cur.execute('''DELETE t1 FROM Tweet t1 INNER JOIN Tweet t2 WHERE t1.date < t2.date AND t1.link = t2.link ''')
         self.db.commit()
 
     def addArticlesFromDf(self, df):
